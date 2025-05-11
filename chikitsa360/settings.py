e16d93112ebd1276658f88a53e27c87bc9bf8a29
siddharthrepo <siddharthraturi12@gmail.com>
2025-05-11 22:46:05 +0530

    cors resolved

--- chikitsa360/settings.py
+++ chikitsa360/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'chat_app.apps.ChatAppConfig',
     'transcription_app.apps.TranscriptionAppConfig',
     'widget_tweaks',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -120,6 +122,25 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True  # or set specific domains
+CORS_ALLOW_METHODS = [
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+]
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'authorization',
+    'content-type',
+    'origin',
+    'x-csrftoken',
+    'x-requested-with',
+]
+
 
 # Custom auth user model
 AUTH_USER_MODEL = 'auth_app.User'
